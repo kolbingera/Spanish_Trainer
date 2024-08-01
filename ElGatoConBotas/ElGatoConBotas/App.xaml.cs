8545ac32548408084e1e423b8917e4a7a63b0af6
kolbingera <annika.kolbinger@muehlbauer.de>
2024-08-01 10:33:01 +0200

    InitialCreate

---
+++ ElGatoConBotas/ElGatoConBotas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ElGatoConBotas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ElGatoConBotas/ElGatoConBotas/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ElGatoConBotas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
